b49d9905b8f076b0477ca51d83551405e978f329
thewilkupl <thewilku@o2.pl>
2021-06-23 14:45:16 +0200

    initial setup

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+        excepturi! Unde sed repellendus harum nihil veritatis eaque consequuntur
+        vitae velit voluptatibus molestias. Aut nemo nihil, repellendus
+        similique veniam magni incidunt fuga saepe fugiat nisi dolor quisquam
+        quis pariatur quasi quam.
+      </p>
+    </section>
+  )
+}
+
+export default About
